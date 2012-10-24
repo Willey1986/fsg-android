be8538b080683425ba108765edce48fc0a53ade6
Willem Almstedt <walmstedt@gmail.com>
2012-10-24 19:39:16 +0200

    Oberfläche

--- src/de/tubs/cs/ibr/fsg/MainActivity.java
+++ src/de/tubs/cs/ibr/fsg/MainActivity.java
@@ -3,13 +3,39 @@ package de.tubs.cs.ibr.fsg;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	SeekBar myBar;
+	TextView text;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView1);
+        text.setText("0");
+        myBar = (SeekBar) findViewById(R.id.seekBar1);
+        myBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				text.setText("" + progress);
+			}
+		});
     }
 
     @Override
