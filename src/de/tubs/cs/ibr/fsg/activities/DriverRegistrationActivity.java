9d94ed02b48f0d1a2b6267fabe433c7d640520a6
Willem <walmstedt@gmail.com>
2012-11-22 21:07:37 +0100

    Datenbank

--- src/de/tubs/cs/ibr/fsg/activities/DriverRegistrationActivity.java
+++ src/de/tubs/cs/ibr/fsg/activities/DriverRegistrationActivity.java
@@ -1,11 +1,18 @@
 package de.tubs.cs.ibr.fsg.activities;
 
+import java.util.Iterator;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import de.tubs.cs.ibr.fsg.R;
+import de.tubs.cs.ibr.fsg.db.models.Driver;
+import de.tubs.cs.ibr.fsg.tasks.ReadFromURLTask;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.*;
 
 public class DriverRegistrationActivity extends Activity {
 
@@ -30,8 +37,30 @@ public class DriverRegistrationActivity extends Activity {
 	}
 	
 	public void refreshDB() {
-		System.out.println("Geklick0rt");
+		try {
+			String stringDrivers = new ReadFromURLTask().execute("http://www.ibr.cs.tu-bs.de/users/poettner/fsg/drivers.php").get();
+			String stringTeams = new ReadFromURLTask().execute("http://www.ibr.cs.tu-bs.de/users/poettner/fsg/teams.php").get();
+			String stringGeneral = new ReadFromURLTask().execute("http://www.ibr.cs.tu-bs.de/users/poettner/fsg/general.php").get();
+			
+			JSONArray jsonDrivers = new JSONArray(stringDrivers);
+			JSONArray jsonTeams = new JSONArray(stringTeams);
+			JSONObject jsonGeneral = new JSONObject(stringGeneral);
+			JSONArray jsonEvents = jsonGeneral.getJSONArray("Classes");
+			
+			for (int i = 0; i <= jsonDrivers.length(); i++) {
+				Driver driver = new Driver(jsonDrivers.getJSONObject(i));
+				System.out.println(driver.toString());
+			}
+			
+			
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
 	}
-
 	
 }
--- src/de/tubs/cs/ibr/fsg/db/models/Driver.java
+++ src/de/tubs/cs/ibr/fsg/db/models/Driver.java
@@ -1,5 +1,8 @@
 package de.tubs.cs.ibr.fsg.db.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Driver {
 	
 	private long id;
@@ -8,6 +11,27 @@ public class Driver {
 	private String first_name;
 	private String last_name;
 	private boolean female;
+	
+	public Driver(JSONObject driver) {
+		try {
+			this.id = 1;
+			this.user_id = driver.getInt("UserID");
+			this.team_id = driver.getInt("TeamID");
+			this.first_name = driver.getString("first_name");
+			this.last_name = driver.getString("last_name");
+			switch (driver.getInt("gender")) {
+				case 0:
+					this.female = false;
+					break;
+				case 1:
+					this.female = true;
+					break;
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	
 	public Driver(long id, int user_id, int team_id, String first_name,
 			String last_name, boolean female) {
@@ -68,6 +92,12 @@ public class Driver {
 		this.female = female;
 	}
 	
-	
+	public String toString() {
+		return this.user_id + "|"
+				+ this.team_id + "|"
+				+ this.first_name + "|"
+				+ this.last_name + "|"
+				+ this.female;
+	}
 
 }
