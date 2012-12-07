80d8c44d5fbd27fa7a006920386ebd5e96623531
Tim Luedtke <t.luedtke@tu-bs.de>
2012-12-07 13:14:17 +0100

    Basisfunktionalität für Infoterminal Actitivity

--- src/de/tubs/cs/ibr/fsg/activities/InfoTerminalActivity.java
+++ src/de/tubs/cs/ibr/fsg/activities/InfoTerminalActivity.java
@@ -1,7 +1,12 @@
 package de.tubs.cs.ibr.fsg.activities;
 
+import java.io.IOException;
+
 import de.tubs.cs.ibr.fsg.R;
 import de.tubs.cs.ibr.fsg.exceptions.FsgException;
+import de.tubs.cs.ibr.fsg.NfcData;
+import de.tubs.cs.ibr.fsg.db.DBAdapter;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,7 +18,17 @@ public class InfoTerminalActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_info_terminal);
 
+		DBAdapter test = new DBAdapter(this);
 		
+		try {
+			NfcData.interpretData(NfcData.generateDataRegistration(test.getDriver(18618)));
+		} catch (FsgException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 	
