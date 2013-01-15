23879a14b2b8acbf305db46bbde7b84331590382
Eduardo2012 <eduardo_sisco@yahoo.de>
2013-01-16 00:02:54 +0100

    InfoTerminal: Erste Schritte...

--- src/de/tubs/cs/ibr/fsg/activities/InfoTerminalActivity.java
+++ src/de/tubs/cs/ibr/fsg/activities/InfoTerminalActivity.java
@@ -1,58 +1,45 @@
 package de.tubs.cs.ibr.fsg.activities;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import de.tubs.cs.ibr.fsg.Nfc;
 import de.tubs.cs.ibr.fsg.NfcData;
-import de.tubs.cs.ibr.fsg.R;
-import de.tubs.cs.ibr.fsg.db.DBAdapter;
-import de.tubs.cs.ibr.fsg.db.models.Driver;
+import de.tubs.cs.ibr.fsg.NfcObject;
+import de.tubs.cs.ibr.fsg.SecurityManager;
 import de.tubs.cs.ibr.fsg.exceptions.FsgException;
 
-public class InfoTerminalActivity extends Activity {
+public class InfoTerminalActivity extends NfcEnabledActivity {
 
+	private Nfc nfc;
+	private SecurityManager scm;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void executeNfcAction(Intent intent) {
 		try {
-			NfcData.interpretData(NfcData.generateCheckIN((short)2));
-			NfcData.interpretData(NfcData.generateCheckOUT((short)2));
-		} catch (Exception  e) {
+			scm = new SecurityManager("geheim");
+			nfc = new Nfc(this);
+			nfc.readTag(intent);
+			byte[][] readedTagContent = nfc.getData();  // Der gelesene Inhalt ist an dieser Stelle noch verschluesselt.
 			
+			byte[][] decryptedContent = scm.decryptString( readedTagContent ); // Hier versuchen wir zu entschluesseln // TODO Hier steigt die APP aus!
+			
+			NfcObject tagContent = NfcData.interpretData( decryptedContent ); // Ohne Verschluesselung koennen wir nun an die Daten
+			
+//			if (tagContent.getDriverObject().getTeam_id() == 0) {
+//				nfc.writeTag(intent, contentToWrite);
+//				txtStatus.setText("Registrierungsdaten geschrieben");
+//			} else {
+//				FsgException e = new FsgException(null, "RegistrationWriteToTagActivity", FsgException.REGISTRATION_ALREADY_PRESENT);
+//				throw e;
+//			}
+		} catch (FsgException e) {
+			Intent mIntent = new Intent(this, ErrorActivity.class);
+			mIntent.putExtra("Exception", e);
+			startActivity(mIntent);
+			finish();
 		}
 		
-		super.onCreate(savedInstanceState);
-		
-		
-                /*
-		setContentView(R.layout.activity_info_terminal);
-		
-		DBAdapter test = new DBAdapter(this);
-		Driver driver = test.getDriver((short)81);
-		
-		ListView listView1 = (ListView) findViewById (R.id.listView1);
-		
-		String[] items = { driver.getFirst_name() , driver.getLast_name(),};
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
-		listView1.setAdapter(adapter);
-		
-
-		ListView listView2 = (ListView) findViewById (R.id.listView2);
-		
-		String[] items1 = { "Rennen1" , "Rennen2", "Rennen3"};
-		
-		ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items1);
-		listView2.setAdapter(adapter1);	
-		
-		ListView listView3 = (ListView) findViewById (R.id.listView3);
-		
-		String[] items11 = { "Briefingstatus"};
-		
-		ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items11);
-		listView3.setAdapter(adapter2);	*/	
-
 	}
+
+
 		
 }
