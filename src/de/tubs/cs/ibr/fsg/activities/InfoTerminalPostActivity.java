c8053784dd3275a8c1999376cea4ce4b3ce7ae2f
Eduardo2012 <eduardo_sisco@yahoo.de>
2013-01-18 18:16:22 +0100

    InfoTerminal: Weitere Fortschritte...

--- src/de/tubs/cs/ibr/fsg/activities/InfoTerminalPostActivity.java
+++ src/de/tubs/cs/ibr/fsg/activities/InfoTerminalPostActivity.java
@@ -1,13 +1,15 @@
 package de.tubs.cs.ibr.fsg.activities;
 
-import de.tubs.cs.ibr.fsg.NfcObject;
-import de.tubs.cs.ibr.fsg.R;
-import de.tubs.cs.ibr.fsg.R.layout;
-import de.tubs.cs.ibr.fsg.R.menu;
-import android.os.Bundle;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.os.Bundle;
+import de.tubs.cs.ibr.fsg.NfcObject;
+import de.tubs.cs.ibr.fsg.R;
 
 public class InfoTerminalPostActivity extends Activity {
 
@@ -17,13 +19,22 @@ public class InfoTerminalPostActivity extends Activity {
         setContentView(R.layout.activity_info_terminal_post);
         
         Intent mIntent = getIntent();
+        
+        
+        Tag tagFromIntent = mIntent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+		MifareClassic tag = MifareClassic.get(tagFromIntent);
+		try {
+			tag.connect();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println("Ist noch dran: " + tag.isConnected());
+        
+        
         NfcObject mNfcObject = mIntent.getParcelableExtra("nfc_object");
         System.out.println(mNfcObject);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_info_terminal_post, menu);
-        return true;
-    }
+
 }
--- src/de/tubs/cs/ibr/fsg/activities/InfoTerminalPreActivity.java
+++ src/de/tubs/cs/ibr/fsg/activities/InfoTerminalPreActivity.java
@@ -1,7 +1,13 @@
 package de.tubs.cs.ibr.fsg.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
 import de.tubs.cs.ibr.fsg.Nfc;
 import de.tubs.cs.ibr.fsg.NfcData;
 import de.tubs.cs.ibr.fsg.NfcObject;
@@ -13,43 +19,20 @@ public class InfoTerminalPreActivity extends NfcEnabledActivity {
 
 	private Nfc nfc;
 	private SecurityManager scm;
-	
+
 	@Override
 	public void executeNfcAction(Intent intent) {
-		try {
-			scm = new SecurityManager("geheim");
-			nfc = new Nfc(this);
-			nfc.readTag(intent);
-			byte[][] readedTagContent = nfc.getData();  // Der gelesene Inhalt ist an dieser Stelle noch verschluesselt.
-			
-//			////////////////////////////////////////////////////////////////
-//			// Block nur fuer die Fehlersuche...
-//			StringBuffer encryptedString = new StringBuffer();
-//			for(int i = 0; i < readedTagContent.length; i++) {
-//				for(int j = 0; j < readedTagContent[i].length; j++) {
-//					encryptedString.append(readedTagContent[i][j]);
-//				}
-//			}
-//			System.out.println("Verschlüsselt: " + encryptedString);
-//			////////////////////////////////////////////////////////////////
-//			
-			//byte[][] decryptedContent = scm.decryptString( readedTagContent ); // Hier versuchen wir zu entschluesseln // TODO Hier steigt die APP aus!
-			
-			NfcObject mNfcObject = NfcData.interpretData( readedTagContent ); // Ohne Verschluesselung koennen wir nun an die Daten
-			
-			System.out.println(mNfcObject); // Nur fuer die Fehlersuche da, hier kann ich beim debuggen stoppen... ;-)
+		TextView msgToUser = (TextView) findViewById(R.id.infoPreNearTextView);
+		Resources res = getResources();
+		msgToUser.setText(res.getString(R.string.infoTerminalReading));
 
-			Intent mIntent = new Intent(this, InfoTerminalPostActivity.class);
-			mIntent.putExtra("nfc_object", mNfcObject);
-			startActivity(mIntent);
+		Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+		intent.putExtra(NfcAdapter.EXTRA_TAG, tagFromIntent);
 			
-		} catch (FsgException e) {
-			Intent mIntent = new Intent(this, ErrorActivity.class);
-			mIntent.putExtra("Exception", e);
-			startActivity(mIntent);
-			finish();
-		}
-		
+			
+		NfcOperation mNfcOperation = new NfcOperation(this, msgToUser);
+		mNfcOperation.execute(intent);
+
 	}
 
 	@Override
@@ -57,9 +40,80 @@ public class InfoTerminalPreActivity extends NfcEnabledActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_info_terminal_pre);
 	}
-	
-	
+
+	private class NfcOperation extends AsyncTask<Intent, Integer, NfcObject> {
+
+		Intent mIntent;
+		Context context;
+		TextView msgToUser;
+
+		public NfcOperation(Context context, TextView msgToUser) {
+			this.context = context;
+			this.msgToUser = msgToUser;
+		}
+
+		@Override
+		protected NfcObject doInBackground(Intent... params) {
+
+			NfcObject mNfcObject = null;
+			try {
+				this.mIntent = params[0];
+				scm = new SecurityManager("geheim");
+				nfc = new Nfc(context);
+				nfc.readTag(mIntent);
+				byte[][] readedTagContent = nfc.getData(); // Der gelesene Inhalt ist an dieser Stelle noch verschluesselt.
 
 
-		
+				// ////////////////////////////////////////////////////////////////
+				// // Block nur fuer die Fehlersuche...
+				// StringBuffer encryptedString = new StringBuffer();
+				// for(int i = 0; i < readedTagContent.length; i++) {
+				// for(int j = 0; j < readedTagContent[i].length; j++) {
+				// encryptedString.append(readedTagContent[i][j]);
+				// }
+				// }
+				// System.out.println("Verschlüsselt: " + encryptedString);
+				// ////////////////////////////////////////////////////////////////
+				//
+				//byte[][] decryptedContent = scm.decryptString( readedTagContent ); // Hier versuchen wir zu entschluesseln // TODO Hier steigt die APP aus!
+
+				mNfcObject = NfcData.interpretData( readedTagContent ); // Ohne Verschluesselung koennen wir nun an die Daten
+
+
+				System.out.println(mNfcObject); // Nur fuer die Fehlersuche da, hier kann ich beim debuggen stoppen... ;-)
+
+				return mNfcObject;
+
+			} catch (FsgException e) {
+				publishProgress(1);
+			}
+			return mNfcObject;
+		}
+
+		@Override
+		protected void onPostExecute(NfcObject mNfcObject) {
+			if (mNfcObject == null) {
+				// Do nothing!
+			} else {
+		        Tag tagFromIntent = this.mIntent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+				Intent mIntent = new Intent(context, InfoTerminalPostActivity.class);
+				mIntent.putExtra("nfc_object", mNfcObject);
+				mIntent.putExtra(NfcAdapter.EXTRA_TAG, tagFromIntent);
+				startActivity(mIntent);
+			}
+		}
+
+		@Override
+		protected void onPreExecute() {
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			if (values[0] == 1) {
+				Resources res = getResources();
+				this.msgToUser.setText(res.getString(R.string.wristband_near));
+			}
+		}
+	}
+
 }
